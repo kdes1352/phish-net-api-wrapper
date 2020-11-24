041501f821d7bdd5c4689e4f680b7724c2ba9365
Keith Deshane <kdes1352@gmail.com>
2020-11-24 10:08:51 -0500

    build shows

--- Phish.Wrapper.Core/Constants.cs
+++ Phish.Wrapper.Core/Constants.cs
@@ -8,11 +8,13 @@
             public const string Appearances = "appearances";
             public const string ByShow = "byshow";
             public const string Get = "get";
+            public const string Links = "links";
             public const string Progress = "progress";
             public const string Query = "query";
             public const string Random = "random";
             public const string Recent = "recent";
             public const string Tiph = "tiph";
+            public const string Upcoming = "upcoming";
         }
     }
 }
